da083b0cf888914b56a6a18ba18cc65bc28ec8c1
alexandremonnet4-svg <alexandre.monnet4@gmail.com>
2026-02-16 14:00:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function btn_shadow() {
+  location.href="https://eliprss.github.io/The-explaining-in-shadow/shadow.html"
+}
+
+function btn_auteur() {
+   location.href="https://eliprss.github.io/The-explaining-in-shadow/auteur.html"
+}
 
