8db1eb01894e8f88032fbef99e466e6029e61018
eliprss <jeuxger2@gmail.com>
2026-02-19 11:56:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,25 +10,23 @@ function note() {
   var reponse = document.getElementById("Id_Reponse").value;
   const selecteur = document.getElementById("choix");
   const monChoix=selecteur[selecteur.selectedIndex];
-if (monChoix.value=="0"){
-  document.getElementById("avis").innerHTML+="<tr><td> 0/5</td><td>"+reponse+"</td></tr>";
-}
-else if (monChoix.value=="1"){
-  document.getElementById("avis").innerHTML+="<tr><td> 1/5</td><td>"+reponse+"</td></tr>";
+if (monChoix.value=="1"){
+  document.getElementById("avis").innerHTML+="<tr><td> <img src=1re.png width=150></td><td>"+reponse+"</td></tr>";
 }  
 else if (monChoix.value=="2"){
-  document.getElementById("avis").innerHTML+="<tr><td> 2/5</td><td>"+reponse+"</td></tr>";
+  document.getElementById("avis").innerHTML+="<tr><td> <img src=2re.png width=150></td><td>"+reponse+"</td></tr>";
 }
 else if (monChoix.value=="3"){
-  document.getElementById("avis").innerHTML+="<tr><td> 3/5</td><td>"+reponse+"</td></tr>";
+  document.getElementById("avis").innerHTML+="<tr><td> <img src=3re.png width=150></td><td>"+reponse+"</td></tr>";
 }
 else if (monChoix.value=="4"){
-  document.getElementById("avis").innerHTML+="<tr><td> 4/5</td><td>"+reponse+"</td></tr>";
+  document.getElementById("avis").innerHTML+="<tr><td> <img src=4re.png width=150></td><td>"+reponse+"</td></tr>";
 }
 else if (monChoix.value=="5"){
-  document.getElementById("avis").innerHTML+="<tr><td> 5/5</td><td>"+reponse+"</td></tr>";
+  document.getElementById("avis").innerHTML+="<tr><td> <img src=5re.png width=150></td><td>"+reponse+"</td></tr>";
 }
 
   
 }
+
 
